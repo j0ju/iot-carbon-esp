4658d6298b819f99106dc6174165454b330f81b0
Joerg Jungermann <j0ju@0x2f.org>
2017-02-05 17:07:30 +0100

    improved up.sh

--- up.sh
+++ up.sh
@@ -3,19 +3,28 @@
 # Author: Joerg Jungermann
 
 DEV=ttyUSB0
-[ -f .port ] && . ./.port
+[ -f .port ] && \
+    . ./.port
 export RSHELL_PORT="/dev/$DEV"
 
-(
-  echo "open $DEV"
-  for py in *.py; do
-    case "$py" in
-      main.py | CONFIG.py ) continue ;;
-    esac
-    echo "put $py"
-    echo $py >&2
-  done
-  echo "ls"
-) | \
-  mpfshell --nocolor --nocache
+FORCE=no
+if [ "$1" = -f ]; then
+    FORCE=yes
+    shift
+fi
 
+(   echo "open $DEV"
+    for py in ${*:-*.py}${*:+"$@"}; do
+        if ! [ "$FORCE" = yes ]; then
+            case "$py" in
+                main.py | CONFIG.py ) continue ;;
+            esac
+        fi
+        echo "put $py"
+        echo "up $py" >&2
+    done
+    echo "ls"
+) | \
+    mpfshell --nocolor --nocache
+
+# vim: ft=sh sw=4 ts=4 et
